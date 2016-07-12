89c4d2df88c471d55ea85ac22ad1b2ed507c2e4c
Tiger Oakes <tigeroakes@gmail.com>
2016-07-12 10:03:16 -0700

    Added computeLength

--- src/compute-length.js
+++ src/compute-length.js
@@ -1,5 +1,5 @@
 import {EARTH_RADIUS} from './utils.js';
-import {convert} from './latlng.js';
+import computeDistanceBetween from './compute-distance-between.js';
 
 /**
  * Returns the length of the given path.
@@ -8,5 +8,8 @@ import {convert} from './latlng.js';
  * @returns {number}
  */
 export default function computeLength(path, radius = EARTH_RADIUS) {
-	
+	let length = 0;
+	for (let i = 0; i < path.length - 1; i++) 
+		length += computeDistanceBetween(path[i], path[i+1], radius);
+	return length;
 }
