071295f77fc56683e1336310f2907ca4e41bf780
Tiger Oakes <contact@tigeroakes.com>
2021-06-02 22:00:10 -1000

    Node 12 test

--- test/data/google-maps.js
+++ test/data/google-maps.js
@@ -1,10 +1,10 @@
-import { readFile } from 'fs/promises';
+import { promises as fs } from 'fs';
 
 /** @type {import('./google-maps.json')} */
 export let googleMaps;
 
 export async function loadGoogleMapsData() {
   const path = new URL('./google-maps.json', import.meta.url);
-  googleMaps = JSON.parse(await readFile(path, 'utf8'));
+  googleMaps = JSON.parse(await fs.readFile(path, 'utf8'));
   return googleMaps;
 }
